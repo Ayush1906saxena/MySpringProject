ff26338b10fb291f62f2bf0a9bc1229091fb4a20
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-10-06 17:09:05 +0530

    Project init

---
+++ src/main/java/com/ayushsaxena/springboot/demo/MyCoolApplication/MyCoolApplication.java
@@ -0,0 +1,13 @@
+package com.ayushsaxena.springboot.demo.MyCoolApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoolApplication.class, args);
+	}
+
+}
